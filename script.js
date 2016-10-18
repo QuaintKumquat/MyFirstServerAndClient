0aabb6fb4c72aef34d7cd2fe4fe640b6ee1b1a4e
QuaintKumquat <jswhitetx@gmail.com>
2016-10-17 21:29:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,26 @@ var sock = null;
 var alive = false;
 
 function start() {
-    sock = new WebSocket("192.168.1.11:14641");
-
-    sock.onopen() = function (e) {
-        document.getElementById("p").innerHTML += "\nOpened!";
+    sock = new WebSocket("ws://192.168.1.14:14641");
+    
+    sock.onopen = function (e) {
+        document.getElementById("p").innerHTML += "<br>Opened!";
+        sock.send("Hello");
     }
 
-    sock.onclose() = function (e) {
-        document.getElementById("p").innerHTML += "\nClosed!";
+    sock.onclose = function (e) {
+        document.getElementById("p").innerHTML += "<br>Closed!";
     }
 
+    sock.onerror = function (e) {
+        document.getElementById("p").innerHTML += "<br>Error!";
+    }
 
     sock.onmessage = function (e) {
-        document.getElementById("p").innerHTML += "\nServer: " + e.data;
+        document.getElementById("p").innerHTML += "<br>Server: " + e.data;
     }
 
 }
-
 
 
 function send(){
